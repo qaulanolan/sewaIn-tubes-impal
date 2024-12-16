0215b2455e6efee4987ce6bf88c95cdb16fca905
qaulan <108211728+qaulanolan@users.noreply.github.com>
2024-12-16 19:20:28 +0700

    Update register.php

--- project/register.php
+++ project/register.php
@@ -81,7 +81,7 @@ if(isset($_POST['submit'])){
       <h3>create an account!</h3>
       <input type="tel" name="name" required maxlength="50" placeholder="enter your name" class="box">
       <input type="email" name="email" required maxlength="50" placeholder="enter your email" class="box">
-      <input type="number" name="number" required min="0" max="999999999999" maxlength="12" placeholder="enter your number" class="box">
+      <input type="number" name="number" required min="0" max="9999999999999" maxlength="13" placeholder="enter your number" class="box">
       <input type="password" name="pass" required maxlength="20" placeholder="enter your password" class="box">
       <input type="password" name="c_pass" required maxlength="20" placeholder="confirm your password" class="box">
       <p>already have an account? <a href="login.php">login now</a></p>
