02267a1385588b5302cd66c077bd40ed6bb07afb
qaulan <108211728+qaulanolan@users.noreply.github.com>
2024-12-16 19:15:19 +0700

    Add files via upload

--- home_db.sql
+++ home_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2024 at 02:30 PM
+-- Generation Time: Dec 16, 2024 at 01:13 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -82,7 +82,8 @@ CREATE TABLE `product` (
 
 INSERT INTO `product` (`id`, `user_id`, `product_name`, `address`, `price`, `type`, `image_01`, `image_02`, `image_03`, `image_04`, `image_05`, `description`, `date`) VALUES
 ('CLHgmSORjF4FnMxRvuLc', '2kjuuLmAbCPEXx6NvUfY', 'W11', 'Bandung', '99999999', 'kendaraan', 'vOW8WUEdHbpSH1w2kQf9.jpg', 'hkpuf34R1hJDKCdqG1oA.jpg', 'v2B7onGlXfHSuo9DTioO.jpg', '', '', 'Mercedes-AMG F1 W11 EQ Performance adalah sebuah mobil balap Formula Satu yang didesain dan dibangun oleh tim Mercedes-AMG Petronas F1 untuk berkompetisi di musim balap Formula Satu 2020. Mobil ini dikenal sangat dominan di musim tersebut dan berhasil membawa tim Mercedes meraih gelar juara dunia konstruktor dan pembalap (diraih oleh Lewis Hamilton).', '2024-12-07'),
-('rYGN7cF8yBCOHleHR2xo', '2kjuuLmAbCPEXx6NvUfY', 'Ferrari F80', 'bubat', '1000', 'kendaraan', '5XuTYwgPJjPKjTPy8S7h.jpg', 'gWJf8pc2vqeynmKrP5Ea.png', 'nlJb21kmOb1VcDU3JUFF.jpg', '', '', 'Ferrari F80 adalah sebuah hypercar yang dirilis oleh pabrikan Italia ternama, Ferrari. Mobil ini hadir sebagai puncak dari inovasi dan teknologi yang dimiliki Ferrari, memadukan performa ekstrem dengan desain yang sangat aerodinamis.', '2024-12-07');
+('rYGN7cF8yBCOHleHR2xo', '2kjuuLmAbCPEXx6NvUfY', 'Ferrari F80', 'bubat', '1000', 'kendaraan', '5XuTYwgPJjPKjTPy8S7h.jpg', 'gWJf8pc2vqeynmKrP5Ea.png', 'nlJb21kmOb1VcDU3JUFF.jpg', '', '', 'Ferrari F80 adalah sebuah hypercar yang dirilis oleh pabrikan Italia ternama, Ferrari. Mobil ini hadir sebagai puncak dari inovasi dan teknologi yang dimiliki Ferrari, memadukan performa ekstrem dengan desain yang sangat aerodinamis.', '2024-12-07'),
+('Dwk1qMDmyfJk6Qy0E7vR', 'pvzRQ5VyBCi3IZ7zWoni', 'tes', 'bdg', '11', 'barang', '40nD44DYcXolxjdvw2dA.png', '', '', '', '', 'tes', '2024-12-16');
 
 -- --------------------------------------------------------
 
@@ -97,6 +98,13 @@ CREATE TABLE `requests` (
   `receiver` varchar(20) NOT NULL,
   `date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `requests`
+--
+
+INSERT INTO `requests` (`id`, `product_id`, `sender`, `receiver`, `date`) VALUES
+('9NmbXfG6twkk9Id2IpJd', 'CLHgmSORjF4FnMxRvuLc', '2kjuuLmAbCPEXx6NvUfY', '2kjuuLmAbCPEXx6NvUfY', '2024-12-16');
 
 -- --------------------------------------------------------
 
@@ -119,7 +127,7 @@ CREATE TABLE `saved` (
 CREATE TABLE `users` (
   `id` varchar(20) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `number` varchar(10) NOT NULL,
+  `number` varchar(13) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `profile_pic` varchar(255) DEFAULT NULL
@@ -130,7 +138,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `number`, `email`, `password`, `profile_pic`) VALUES
-('2kjuuLmAbCPEXx6NvUfY', 'a', '123', 'a@gmail.com', 'f10e2821bbbea527ea02200352313bc059445190', '6753f6737b571_me.png');
+('2kjuuLmAbCPEXx6NvUfY', 'a', '123', 'a@gmail.com', 'f10e2821bbbea527ea02200352313bc059445190', '6753f6737b571_me.png'),
+('pvzRQ5VyBCi3IZ7zWoni', 'olan', '81394234964', 'olan2@gmail.com', 'f10e2821bbbea527ea02200352313bc059445190', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
