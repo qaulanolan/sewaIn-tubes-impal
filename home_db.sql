ff11f35642cbfc323ec3244a42a28cf74cbedb55
TigorBekerja <120919621+TigorBekerja@users.noreply.github.com>
2024-12-31 00:19:02 +0700

    Add files via upload

--- home_db.sql
+++ home_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 29, 2024 at 05:02 AM
+-- Generation Time: Dec 30, 2024 at 06:17 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -68,6 +68,9 @@ CREATE TABLE `product` (
   `address` varchar(300) NOT NULL,
   `price` varchar(10) NOT NULL,
   `type` varchar(10) NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `sub_category` varchar(50) NOT NULL,
+  `specific_category` varchar(50) NOT NULL,
   `image_01` varchar(50) NOT NULL,
   `image_02` varchar(50) NOT NULL,
   `image_03` varchar(50) NOT NULL,
@@ -76,6 +79,13 @@ CREATE TABLE `product` (
   `description` varchar(1000) NOT NULL,
   `date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `user_id`, `product_name`, `address`, `price`, `type`, `category`, `sub_category`, `specific_category`, `image_01`, `image_02`, `image_03`, `image_04`, `image_05`, `description`, `date`) VALUES
+(5, 2, 'mio', 'Bandung', '12345', 'kendaraan', 'kendaraan roda dua', 'motor', 'motor matic', 'mGluCkKsyIptjn7FGryN.jpg', '', '', '', '', '12345', '2024-12-31');
 
 -- --------------------------------------------------------
 
@@ -117,6 +127,13 @@ CREATE TABLE `users` (
   `password` varchar(50) NOT NULL,
   `profile_pic` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `number`, `email`, `password`, `profile_pic`) VALUES
+(2, '123', '123', '123@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', NULL);
 
 --
 -- Indexes for dumped tables
@@ -166,37 +183,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `saved`
 --
 ALTER TABLE `saved`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
