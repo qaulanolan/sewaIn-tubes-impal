90b295f8d6b4fde5dd6d9efbb6de9b667279d1e4
TigorBekerja <120919621+TigorBekerja@users.noreply.github.com>
2024-12-31 22:11:56 +0700

    Add files via upload

--- project/register.php
+++ project/register.php
@@ -30,7 +30,7 @@ if(isset($_POST['submit'])){
       if($pass != $c_pass){
          $warning_msg[] = 'Password not matched!';
       }else{
-         $insert_user = $conn->prepare("INSERT INTO `users`(name, number, email, password) VALUES(?,?,?,?,?)");
+         $insert_user = $conn->prepare("INSERT INTO `users`(name, number, email, password) VALUES(?,?,?,?)");
          $insert_user->execute([$name, $number, $email, $c_pass]);
          
          if($insert_user){
@@ -80,7 +80,7 @@ if(isset($_POST['submit'])){
       <h3>create an account!</h3>
       <input type="tel" name="name" required maxlength="50" placeholder="enter your name" class="box">
       <input type="email" name="email" required maxlength="50" placeholder="enter your email" class="box">
-      <input type="number" name="number" required min="0" max="9999999999999" maxlength="13" placeholder="enter your number" class="box">
+      <input type="number" name="number" required min="080000000000" max="9999999999999" maxlength="13" placeholder="enter your number" class="box">
       <input type="password" name="pass" required maxlength="20" placeholder="enter your password" class="box">
       <input type="password" name="c_pass" required maxlength="20" placeholder="confirm your password" class="box">
       <p>already have an account? <a href="login.php">login now</a></p>
